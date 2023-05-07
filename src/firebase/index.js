a787e38b801bbff1e2631fa08145c3059306d3f4
Priyanshu <priyanshugevra@gmail.com>
2023-05-07 22:24:40 +0530

    add: init

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -15,5 +14,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 export const db = getFirestore(app);
