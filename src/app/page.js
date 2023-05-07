1d46b0aa43300454fb92c673a7ebf2b64b9e4e73
Priyanshu <priyanshugevra@gmail.com>
2023-05-07 22:19:39 +0530

    add: init

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,207 @@
-import Image from 'next/image'
+"use client"
+import {BsCreditCard2Front, BsCalendar3} from "react-icons/bs";
+import {TbFileReport} from "react-icons/tb"
+import {FaHistory, FaStar} from "react-icons/fa"
+
+import {Swiper, SwiperSlide} from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
+import {Autoplay} from "swiper";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    const services = [
+        {
+            icon: <BsCreditCard2Front className={"text-4xl"}/>,
+            title: "Smart Card",
+            description: "provides a digital card that stores and manages personal information such as identification and payment details."
+        },
+        {
+            icon: <BsCalendar3 className={"text-4xl"}/>,
+            title: "Make Appointment",
+            description: "allows users to schedule appointments with healthcare providers or other service providers."
+        },
+        {
+            icon: <TbFileReport className={"text-4xl"}/>,
+            title: "Medical Report",
+            description: "provides access to digital reports that contain medical test results and other healthcare-related information."
+        },
+        {
+            icon: <FaHistory className={"text-4xl"}/>,
+            title: "Medical History\n",
+            description: "allows users to view their medical history, including previous diagnoses, treatments, and medications."
+        },
+    ]
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    const doctors = [
+        {
+            image: "https://randomuser.me/api/portraits/men/86.jpg",
+            name: "Dr.John Deo",
+            category: "Cardiologists",
+            rating: 4.5,
+            reviews: 56
+        },
+        {
+            image: "https://randomuser.me/api/portraits/women/42.jpg",
+            name: "Dr. Jane Smith",
+            category: "Dermatologists",
+            rating: 4.9,
+            reviews: 102
+        },
+        {
+            image: "https://randomuser.me/api/portraits/men/72.jpg",
+            name: "Dr. Mark Johnson",
+            category: "Oncologists",
+            rating: 4.2,
+            reviews: 34
+        },
+        {
+            image: "https://randomuser.me/api/portraits/women/91.jpg",
+            name: "Dr. Emily Brown",
+            category: "Pediatricians",
+            rating: 4.7,
+            reviews: 76
+        },
+        {
+            image: "https://randomuser.me/api/portraits/women/66.jpg",
+            name: "Dr. Rachel Lee",
+            category: "Psychiatrists",
+            rating: 4.3,
+            reviews: 28
+        },
+        {
+            image: "https://randomuser.me/api/portraits/men/55.jpg",
+            name: "Dr. Michael Chen",
+            category: "Gastroenterologists",
+            rating: 4.8,
+            reviews: 89
+        },
+        {
+            image: "https://randomuser.me/api/portraits/women/13.jpg",
+            name: "Dr. Sophia Rodriguez",
+            category: "Endocrinologists",
+            rating: 4.6,
+            reviews: 44
+        }
+    ]
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+    return (
+        <main className="flex flex-col items-center justify-between">
+            <div className={"w-full container"}>
+                <img src={"/home-bg.png"} alt={"background"} className={"w-full object-cover rounded-b-xl"}/>
+            </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+            {/*Our Services*/}
+            <section className="bg-white dark:bg-gray-900">
+                <div className="container px-6 py-10 mx-auto">
+                    <h1 className=" text-center text-2xl font-semibold text-gray-800 capitalize lg:text-3xl dark:text-white">
+                        our <span className="underline decoration-blue-500">Services</span>
+                    </h1>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+                    <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-2 xl:grid-cols-4">
+
+                        {
+                            services.map((service, index) => {
+                                return (
+                                    <div
+                                        className="p-8 space-y-3 border-2 border-blue-400 dark:border-blue-300 rounded-xl"
+                                        key={index}>
+                      <span className="inline-block text-blue-500 dark:text-blue-400">
+                        {service.icon}
+                      </span>
+
+                                        <h1 className="text-xl font-semibold text-gray-700 capitalize dark:text-white">{service.title}</h1>
+
+                                        <p className="text-gray-500 dark:text-gray-300">{service.description}</p>
+
+                                    </div>
+                                )
+                            })
+                        }
+
+
+                    </div>
+                </div>
+            </section>
+
+
+            {/*  Our Doctors */}
+            <section className="bg-white dark:bg-gray-900">
+                <div className="container px-6 py-10 mx-auto flex flex-col">
+                    <h1 className="text-2xl font-semibold text-center text-gray-800 capitalize lg:text-3xl dark:text-white">
+                        Our Doctors
+                    </h1>
+
+                    <p className="max-w-2xl mx-auto my-6 text-center text-gray-500 dark:text-gray-300">
+                        Specialized Doctors from 100+ Hospitals
+                    </p>
+
+                    <div className="container w-full max-w-[85vw] ">
+                        <Swiper
+                            slidesPerView={1}
+                            spaceBetween={10}
+                            breakpoints={{
+                                770:{
+                                    slidesPerView:2,
+                                    spaceBetween:20
+                                },
+                                1080:{
+                                    slidesPerView:3,
+                                    spaceBetween:40
+                                }
+                            }}
+                            autoplay={{
+                                delay: 1500,
+                                disableOnInteraction: false,
+                                stopOnLastSlide:false,
+                                reverseDirection: true
+                            }}
+                            modules={[Autoplay]}
+                            loop={true}
+                            grabCursor={true}
+                            className="mySwiper mx-auto px-4 md:max-w-auto"
+                        >
+                            {
+                                doctors.map((doctor, index) => {
+                                    return (
+                                        <SwiperSlide key={index}>
+                                            <div className="col-span-1 flex flex-col items-center p-8 border rounded-xl ">
+                                                <img
+                                                    className="object-cover w-32 h-32 rounded-full ring-4 ring-gray-300"
+                                                    src={doctor.image}
+                                                    alt={doctor.name}/>
+
+                                                <h1 className="mt-4 text-2xl font-semibold text-gray-700 capitalize dark:text-white group-hover:text-white">
+                                                    {doctor.name}
+                                                </h1>
+
+                                                <p className="mt-2 text-gray-500 capitalize dark:text-gray-300 group-hover:text-gray-300">
+                                                    {doctor.category}
+                                                </p>
+
+                                                <div className="flex items-center my-1 group-hover:text-gray-100">
+                                                    <FaStar className={"text-amber-500"}/>
+                                                    <p className="ml-2 text-sm font-bold">{doctor.rating}</p>
+                                                    <span className="w-1 h-1 mx-1.5 rounded-full "></span>
+                                                    <a href="#"
+                                                       className="text-sm font-medium underline hover:no-underline ">
+                                                        {doctor.reviews}
+                                                        reviews</a>
+                                                </div>
+                                            </div>
+                                        </SwiperSlide>
+                                    )
+
+                                })
+                            }
+                        </Swiper>
+
+                    </div>
+                </div>
+            </section>
+
+        </main>
+    )
 }
---
+++ src/firebase/index.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD5Kj0SIjSX3_cuYn35r3nTg2DySUkJ6RU",
+    authDomain: "e-clinic-6139c.firebaseapp.com",
+    projectId: "e-clinic-6139c",
+    storageBucket: "e-clinic-6139c.appspot.com",
+    messagingSenderId: "351608813768",
+    appId: "1:351608813768:web:f43d641806282f518d4dfd",
+    measurementId: "G-RLWRHCRRN6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const db = getFirestore(app);
